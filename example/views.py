3b8087329ff155a7e493029a38fc8e03fc88c92a
HassanAbbas7 <129033939+HassanAbbas7@users.noreply.github.com>
2023-08-31 13:34:24 +0500

    Update views.py

--- example/views.py
+++ example/views.py
@@ -11,21 +11,19 @@ def index(request):
         yt = YouTube(link)
     except:
         print("Connection Error") #to handle exception
-    
-    # filters out all the files with "mp4" extension
-    stream = yt.streams.get_highest_resolution()
-    stream.download("/tmp/")
-    with open(glob.glob("/tmp/*.mp4")[0], "rb") as f:
-        video_data = f.read()
 
-
-    response = HttpResponse(video_data, content_type="video/mp4")
-    response["Content-Disposition"] = f"attachment; filename='myfile.mp4'"
-    #to set the name of the file
-    
-    # get the video with the extension and
-    print('Task Completed!')
     try:
-        return response
-    except Exception as e:
+        # filters out all the files with "mp4" extension
+        stream = yt.streams.get_highest_resolution()
+        stream.download("/tmp/")
+        with open(glob.glob("/tmp/*.mp4")[0], "rb") as f:
+            video_data = f.read()
+    
+    
+        response = HttpResponse(video_data, content_type="video/mp4")
+        response["Content-Disposition"] = f"attachment; filename='myfile.mp4'"
+        #to set the name of the file
         return HttpResponse("good")
+    except Exception as e:
+        print(e)
+        return HttpResponse("error")
