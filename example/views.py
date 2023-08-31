8f3fa460376e02816f8ce15a9abc230602fe3879
HassanAbbas7 <129033939+HassanAbbas7@users.noreply.github.com>
2023-08-31 13:20:03 +0500

    Update views.py

--- example/views.py
+++ example/views.py
@@ -14,10 +14,14 @@ def index(request):
     
     # filters out all the files with "mp4" extension
     stream = yt.streams.get_highest_resolution()
-    stream.download("/tmp/")
-    
+    stream.download(output_path="/tmp/", filename="myfile")
+    with open(f"/tmp/myfile.mp4", "rb") as f:
+        video_data = f.read()
+        
+    response = HttpResponse(video_data, content_type="video/mp4")
+    response["Content-Disposition"] = f"attachment; filename='myfile.mp4'"
     #to set the name of the file
     
     # get the video with the extension and
     print('Task Completed!')
-    return HttpResponse("success")
+    return response
